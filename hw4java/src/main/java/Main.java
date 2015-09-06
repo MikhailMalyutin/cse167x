3522dffc60808cb6a588428db0ba1ed1ef6b7deb
mmalutin <mmalutin@flexis.ru>
2015-09-06 18:50:06 +0300

    Начальная конфигурация проекта

--- hw4java/src/main/java/Main.java
+++ hw4java/src/main/java/Main.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 import static tracer.RayTracer.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String fileName = "scene1.test";
+    public static void main(String[] args) throws Exception {
+        String fileName = "j:\\Work\\Learn\\cse167x1\\testscenes\\scene1.test";
         Model model = SceneParser.parse(new File(fileName));
         BufferedImage image = render(model);
-        File outFileName = new File("out.png");
+        File outFileName = new File("j:\\Work\\Learn\\cse167x1\\testscenes\\out.png");
         ImageIO.write(image, "png", outFileName);
     }
 }
--- hw4java/src/main/java/parser/SceneParser.java
+++ hw4java/src/main/java/parser/SceneParser.java
@@ -1,12 +1,23 @@
 package parser;
 
+import org.apache.commons.io.FileUtils;
 import scene.Model;
 
 import java.io.File;
 
 public class SceneParser {
-    public static Model parse(File file) {
+    public static Model parse(File file) throws Exception {
         Model result = new Model();
+        String str = FileUtils.readFileToString(file);
+        String[] lines = str.split("\n");
+        for (String line : lines) {
+            String[] commands = line.split(" ");
+            final String operator = commands[0].trim();
+            if (operator.equals("size")) {
+                result.setW(Integer.parseInt(commands[1]));
+                result.setH(Integer.parseInt(commands[2]));
+            }
+        }
 
         return result;
     }
