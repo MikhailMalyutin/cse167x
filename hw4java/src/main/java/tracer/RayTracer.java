b999b7827135e0f79e8f29d849abaceba2c22005
mmalutin <mmalutin@flexis.ru>
2015-09-06 18:56:19 +0300

    Начальная конфигурация проекта

--- hw4java/src/main/java/tracer/RayTracer.java
+++ hw4java/src/main/java/tracer/RayTracer.java
@@ -6,7 +6,12 @@ import java.awt.image.BufferedImage;
 
 public class RayTracer {
     public static BufferedImage render(Model model) {
-        BufferedImage result = new BufferedImage(model.getW(), model.getH(), BufferedImage.TYPE_4BYTE_ABGR);
+        BufferedImage result = new BufferedImage(model.getW(), model.getH(), BufferedImage.TYPE_INT_RGB);
+        for (int x=0; x< model.getW(); ++x) {
+            for (int y=0; y < model.getH(); ++y) {
+                result.setRGB(x, y, 255 * 16*16 + 128);
+            }
+        }
         return result;
     }
 }
