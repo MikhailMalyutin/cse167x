c62841fd8c4e6f73d435312a2d47839584fbb039
mmalutin <mmalutin@flexis.ru>
2015-09-07 16:10:10 +0300

    Начальная конфигурация проекта

--- hw4java/src/main/java/tracer/RayTracer.java
+++ hw4java/src/main/java/tracer/RayTracer.java
@@ -6,12 +6,37 @@ import java.awt.image.BufferedImage;
 
 public class RayTracer {
     public static BufferedImage render(Model model) {
+        Camera cam = getCamera(model);
         BufferedImage result = new BufferedImage(model.getW(), model.getH(), BufferedImage.TYPE_INT_RGB);
         for (int x=0; x< model.getW(); ++x) {
             for (int y=0; y < model.getH(); ++y) {
-                result.setRGB(x, y, 255 * 16*16 + 128);
+                Ray ray = rayThruPixel(cam, x, y);
+                Intersection hit = intersect(ray, model);
+                result.setRGB(x, y, findColor(hit));
             }
         }
+        return result;
+    }
+
+    private static int findColor(Intersection hit) {
+        return 255 * 16*16 + 128;
+    }
+
+    private static Intersection intersect(Ray ray, Model model) {
+        Intersection result = new Intersection();
+
+        return result;
+    }
+
+    private static Ray rayThruPixel(Camera cam, int x, int y) {
+        Ray result = new Ray();
+
+        return result;
+    }
+
+    private static Camera getCamera(Model model) {
+        Camera result = new Camera();
+
         return result;
     }
 }
