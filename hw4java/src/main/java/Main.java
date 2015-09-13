f028efaec37798dcd82298dc55f30ff190edaf0f
mmalutin <mmalutin@flexis.ru>
2015-09-13 11:01:21 +0300

    Реализация

--- hw4java/src/main/java/Main.java
+++ hw4java/src/main/java/Main.java
@@ -11,10 +11,26 @@ import static tracer.RayTracer.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String fileName = "j:\\Work\\Learn\\cse167x1\\testscenes\\scene3.test";
-        Model model = SceneParser.parse(new File(fileName));
+//        String fileName = "j:\\Work\\Learn\\cse167x1\\testscenes\\scene3.test";
+//        Model model = SceneParser.parse(new File(fileName));
+//        BufferedImage image = render(model);
+//        File outFileName = new File("j:\\Work\\Learn\\cse167x1\\testscenes\\out.png");
+//        ImageIO.write(image, "png", outFileName);
+        renderFile("scene4-ambient");
+        renderFile("scene4-diffuse");
+        renderFile("scene4-emission");
+        renderFile("scene4-specular");
+        renderFile("scene5");
+        renderFile("scene6");
+        renderFile("scene7");
+    }
+
+    private static void renderFile(String fileName) throws Exception {
+        final String workDir = "j:\\Work\\Learn\\cse167x1\\testscenes\\";
+        String filePath = workDir + fileName + ".test";
+        Model model = SceneParser.parse(new File(filePath));
         BufferedImage image = render(model);
-        File outFileName = new File("j:\\Work\\Learn\\cse167x1\\testscenes\\out.png");
+        File outFileName = new File(workDir + fileName + ".png");
         ImageIO.write(image, "png", outFileName);
     }
 }
